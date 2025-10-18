18e17f83ef57e9edeb35d969725063511bcd6d4c
Vladislav Kutnyak <kutnyak.03@mail.ru>
2025-10-18 11:49:56 +0300

    вернул файлы калькулатора

--- calculator/mainwindow.cpp
+++ calculator/mainwindow.cpp
@@ -1,407 +1,326 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "algo.h"
+#include "controller.h"
 
-#include <QMessageBox>
-#include <algorithm>
-#include <QFileDialog>
-#include <QFile>
-#include <QTextStream>
-#include <cctype>
+#include <QDebug>
 
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow) {
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+
     ui->setupUi(this);
 
-    ApplyModel();
+    ui->cmb_controller->clear();
+    ui->cmb_controller->addItems({
+        "double",
+        "float",
+        "uint8_t",
+        "int",
+        "int64_t",
+        "size_t",
+        "Rational"
+    });
+
+    ui->cmb_controller->setFixedSize(100, 25);
+
+    ui->l_result->setText("0");
+    ui->l_memory->setText("");
+    ui->l_formula->setText("");
+
 }
 
 MainWindow::~MainWindow() {
     delete ui;
 }
 
-void MainWindow::SetRandomGen(const std::mt19937 &random_gen){
-    random_gen_ = random_gen;
+
+//Установка значений в калькулятор
+void MainWindow::SetInputText(const std::string &text){
+    ui->l_result->setStyleSheet("");
+    ui->l_result->setText(QString::fromStdString(text));
 }
 
 
-
-
-
-
-//Методы
-void MainWindow::on_btn_clear_clicked(){
-    deque_model_.items.clear();
-    ApplyModel();
-
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
+void MainWindow::SetErrorText(const std::string &text){
+    ui->l_result->setStyleSheet("color: red;");
+    ui->l_result->setText(QString::fromStdString(text));
 }
 
 
-void MainWindow::on_btn_push_front_clicked(){
-    std::string set_text = ui->txt_elem_content->text().toStdString();
-
-    deque_model_.items.push_front(set_text);
-    ApplyModel();
-
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
-}
-
-void MainWindow::on_btn_pop_front_clicked(){
-    deque_model_.items.pop_front();
-    ApplyModel();
-
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
+void MainWindow::SetFormulaText(const std::string &text){
+    ui->l_formula->setText(QString::fromStdString(text));
 }
 
 
-void MainWindow::on_btn_push_back_clicked(){
-    std::string set_text = ui->txt_elem_content->text().toStdString();
-
-    deque_model_.items.push_back(set_text);
-    ApplyModel();
-
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
+void MainWindow::SetMemText(const std::string &text){
+    ui->l_memory->setText(QString::fromStdString(text));
 }
 
 
-void MainWindow::on_btn_pop_back_clicked(){
-    deque_model_.items.pop_back();
-    ApplyModel();
-
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
-}
-
-void MainWindow::on_btn_insert_clicked(){
-    deque_model_.items.insert(deque_model_.iterator, ui->txt_elem_content->text().toStdString());
-    ApplyModel();
-
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
-}
-
-void MainWindow::on_btn_erase_clicked(){
-    deque_model_.items.erase(deque_model_.iterator);
-    ApplyModel();
-
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
-}
-
-
-
-
-
-
-
-
-//Алгоритмы
-void MainWindow::on_btn_lower_bound_clicked(){
-    if(!std::is_sorted(deque_model_.items.begin(),deque_model_.items.end())){
+void MainWindow::SetExtraKey(const std::optional<std::string> &key){
+    if(!key.has_value()){
+        ui->tb_extra->setVisible(false);
         return;
     }
 
-    deque_model_.iterator = std::lower_bound(deque_model_.items.begin(),
-                                             deque_model_.items.end(),
-                                             ui->txt_elem_content->text().toStdString());
-
-    ApplyIterator();
-}
-
-void MainWindow::on_btn_upper_bound_clicked(){
-    if(!std::is_sorted(deque_model_.items.begin(),deque_model_.items.end())){
-        return;
-    }
-
-    deque_model_.iterator = std::upper_bound(deque_model_.items.begin(),
-                                             deque_model_.items.end(),
-                                             ui->txt_elem_content->text().toStdString());
-
-    ApplyIterator();
-}
-
-void MainWindow::on_btn_min_el_clicked(){
-    auto it = std::min_element(deque_model_.items.begin(), deque_model_.items.end());
-
-    if(it != deque_model_.items.end()){
-        deque_model_.iterator = it;
-        deque_model_.iterator = it;
-    }
-
-    ApplyIterator();
-}
-
-void MainWindow::on_btn_max_el_clicked(){
-    auto it = std::max_element(deque_model_.items.begin(), deque_model_.items.end());
-
-    if(it != deque_model_.items.end()){
-        deque_model_.iterator = it;
-    }
-
-    ApplyIterator();
-}
-
-void MainWindow::on_btn_sort_clicked(){
-    deque_model_.items = MergeSort(deque_model_.items, std::less<>());
-    ApplyModel();
-
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
+    ui->tb_extra->setVisible(true);
+    ui->tb_extra->setText(QString::fromStdString(key.value()));
 }
 
 
-void MainWindow::on_btn_sOrT_clicked(){
-    auto compare_ci = [](const std::string& l, const std::string& r) {
-        return QString::compare(QString::fromStdString(l), QString::fromStdString(r), Qt::CaseInsensitive) < 0;
-    };
 
-    deque_model_.items = MergeSort(deque_model_.items, compare_ci);
 
-    ApplyModel();
 
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
+// Callback операторы
+void MainWindow::SetDigitKeyCallback(std::function<void (int)> cb){
+    digit_cb_ = cb;
 }
 
-void MainWindow::on_btn_unique_clicked(){
-    if(std::is_sorted(deque_model_.items.begin(), deque_model_.items.end())){
-        auto to_del = std::unique(deque_model_.items.begin(), deque_model_.items.end());
-        deque_model_.items.erase(to_del, deque_model_.items.end());
+void MainWindow::SetProcessOperationKeyCallback(std::function<void (Operation)> cb){
+    operation_cb_ = cb;
+}
 
-        deque_model_.iterator = deque_model_.items.begin();
+void MainWindow::SetProcessControlKeyCallback(std::function<void (ControlKey)> cb){
+    control_cb_ = cb;
+}
 
-        ApplyModel();
-        ApplyIterator();
+void MainWindow::SetControllerCallback(std::function<void (ControllerType)> cb){
+    controller_cb_ = cb;
+}
+
+
+
+
+// Обработка кнопок с цифрами
+void MainWindow::on_tb_zero_clicked(){
+    if (digit_cb_) {
+        digit_cb_(0);
     }
 }
 
 
-void MainWindow::on_btn_reverse_clicked(){
-    std::reverse(deque_model_.items.begin(), deque_model_.items.end());
-
-    ApplyModel();
-    ApplyIterator();
-}
-
-
-void MainWindow::on_btn_shuffle_clicked(){
-    // Создание генератора со случайным начальным значением.
-    std::mt19937 gen_shuffle(random_gen_);
-    SetRandomGen(gen_shuffle);
-
-
-    // Перемешаем элементы в векторе.
-    std::shuffle(deque_model_.items.begin(), deque_model_.items.end(), gen_shuffle);
-
-
-    ApplyModel();
-    ApplyIterator();
-}
-
-
-
-
-
-
-
-
-
-//Итераторы
-void MainWindow::on_btn_dec_iterator_clicked(){
-    if (deque_model_.iterator != deque_model_.items.begin()) {
-        deque_model_.iterator -= 1;
-        ApplyIterator();
+void MainWindow::on_tb_one_clicked(){
+    if (digit_cb_) {
+        digit_cb_(1);
     }
 }
 
-void MainWindow::on_btn_inc_iterator_clicked(){
-    if (deque_model_.iterator != deque_model_.items.end()) {
-        deque_model_.iterator += 1;
-        ApplyIterator();
+
+void MainWindow::on_tb_two_clicked(){
+    if (digit_cb_) {
+        digit_cb_(2);
     }
 }
 
-void MainWindow::on_btn_begin_clicked(){
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
+
+void MainWindow::on_tb_three_clicked(){
+    if (digit_cb_) {
+        digit_cb_(3);
+    }
 }
 
 
-void MainWindow::on_btn_end_clicked(){
-    deque_model_.iterator = deque_model_.items.end();
-
-    ApplyIterator();
+void MainWindow::on_tb_four_clicked(){
+    if (digit_cb_) {
+        digit_cb_(4);
+    }
 }
 
 
-
-
-
-//Заготовки
-void MainWindow::on_btn_tea_clicked(){
-    deque_model_.items = tea;
-    ApplyModel();
-
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
-}
-
-void MainWindow::on_btn_cakes_clicked(){
-    deque_model_.items = cakes;
-    ApplyModel();
-
-    deque_model_.iterator = deque_model_.items.begin();
-    ApplyIterator();
+void MainWindow::on_tb_five_clicked(){
+    if (digit_cb_) {
+        digit_cb_(5);
+    }
 }
 
 
-
-
-//Другие кнопки работы с очередью
-void MainWindow::on_btn_edit_clicked(){
-    int index = std::distance(deque_model_.items.begin(),deque_model_.iterator);
-    std::string text = ui->txt_elem_content->text().toStdString();
-    deque_model_.items[index] = text;
-
-    ApplyModel();
-    ApplyIterator();
+void MainWindow::on_tb_six_clicked(){
+    if (digit_cb_) {
+        digit_cb_(6);
+    }
 }
 
-void MainWindow::on_btn_resize_clicked(){
-    int text_from_txt_size = ui->txt_size->text().toInt();
-    deque_model_.items.resize(text_from_txt_size);
-    deque_model_.iterator = deque_model_.items.begin();
-
-    ApplyModel();
-    ApplyIterator();
+void MainWindow::on_tb_seven_clicked(){
+    if (digit_cb_) {
+        digit_cb_(7);
+    }
 }
 
 
-void MainWindow::on_btn_find_clicked(){
-    std::string text = ui->txt_elem_content->text().toStdString();
-    auto it = std::find(deque_model_.items.begin(), deque_model_.items.end(), text);
-    deque_model_.iterator = it;
-
-
-    ApplyIterator();
+void MainWindow::on_tb_eight_clicked(){
+    if (digit_cb_) {
+        digit_cb_(8);
+    }
 }
 
 
-void MainWindow::on_btn_count_clicked(){
-    int count = std::count(deque_model_.items.begin(), deque_model_.items.end(), ui->le_count->text().toStdString());
-
-    ui->lbl_count->setText(QString::number(count));
+void MainWindow::on_tb_nine_clicked(){
+    if (digit_cb_) {
+        digit_cb_(9);
+    }
 }
 
 
 
 
-//Методы применения изменений
-void MainWindow::ApplyModel() {
-    if(deque_model_.items.empty()){
-        ui->list_widget->clear();
-        ui->txt_size->setText(QString::number(0));
-        ui->txt_elem_content->clear();
-        ui->list_widget->addItem("end");
 
-        SetAbleDisableBtn();
-        return;
+// Обработка других кнопок ввода
+void MainWindow::on_tb_extra_clicked()
+{
+    if(control_cb_){
+        control_cb_(ControlKey::EXTRA_KEY);
+    }
+}
+
+void MainWindow::on_tb_negate_clicked()
+{
+    if(control_cb_){
+        control_cb_(ControlKey::PLUS_MINUS);
+    }
+}
+
+void MainWindow::on_tb_backspace_clicked()
+{
+    if(control_cb_){
+        control_cb_(ControlKey::BACKSPACE);
     }
 
-    int size_deque = deque_model_.items.size();
-    ui->txt_size->setText(QString::number(size_deque));
-
-
-    auto preserve_iter = deque_model_.iterator;
-    ui->list_widget->clear();
-
-    for(int i = 0; i < size_deque; ++i){
-        ui->list_widget->addItem(QString::number(i) + ": " + QString::fromStdString(deque_model_.items[i]));
-    }
-
-    ui->list_widget->addItem("end");
-    deque_model_.iterator = preserve_iter;
-
-    SetAbleDisableBtn();
 }
 
-void MainWindow::ApplyIterator(){
-    if (deque_model_.items.empty()) {
-        ui->list_widget->setCurrentRow(0); // Выделяем "end"
-        ui->txt_elem_content->clear();
 
-    } else {
-        int hops = std::distance(deque_model_.items.begin(), deque_model_.iterator);
-        ui->list_widget->setCurrentRow(hops);
 
-        if(deque_model_.iterator == deque_model_.items.end()){
-            ui->txt_elem_content->clear();
-        } else{
-            ui->txt_elem_content->setText(QString::fromStdString(*deque_model_.iterator));
+
+// Нормализация чисел
+QString MainWindow::RemoveTrailingZeroes(const QString &text) {
+    for (qsizetype i = 0; i < text.size(); ++i) {
+        if (text[i] != '0') {
+            return text.mid(i);
         }
     }
-
-    SetAbleDisableBtn();
-}
-
-void MainWindow::SetAbleDisableBtn(){
-    bool iterator_end = deque_model_.iterator == deque_model_.items.end();
-    bool iterator_begin = deque_model_.iterator == deque_model_.items.begin();
-    bool deque_is_empty = deque_model_.items.empty();
-
-
-    ui->btn_pop_back->setDisabled(deque_is_empty);
-    ui->btn_pop_front->setDisabled(deque_is_empty);
-    ui->btn_erase->setDisabled(iterator_end);
-
-
-    ui->btn_dec_iterator->setDisabled(iterator_begin);
-    ui->btn_inc_iterator->setDisabled(iterator_end);
-
-
-    ui->btn_begin->setDisabled(iterator_begin);
-    ui->btn_end->setDisabled(iterator_end);
-
-
-    ui->btn_edit->setDisabled(iterator_end);
+    return "";
 }
 
 
-
-
-void MainWindow::on_list_widget_currentRowChanged(int currentRow){
-    int current_row = std::clamp(currentRow, 0, (int)deque_model_.items.size());
-    int deque_size = static_cast<int>(deque_model_.items.size());
-
-
-    if (current_row < deque_size) {
-        deque_model_.iterator = deque_model_.items.begin() + current_row;
-
-    } else if (current_row == deque_size) {
-        deque_model_.iterator = deque_model_.items.end();
-
+QString MainWindow::NormalizeNumber(const QString &text) {
+    if (text.isEmpty()) {
+        return "0";
     }
-
-    ApplyIterator();
+    if (text.startsWith('.')) {
+        // Рекурсивный вызов.
+        return NormalizeNumber("0" + text);
+    }
+    if (text.startsWith('-')) {
+        // Рекурсивный вызов.
+        return "-" + NormalizeNumber(text.mid(1));
+    }
+    if (text.startsWith('0') && !text.startsWith("0.")) {
+        return NormalizeNumber(RemoveTrailingZeroes(text));
+    }
+    return text;
 }
 
 
 
 
 
+//Кнопки операций
+void MainWindow::on_tb_power_clicked(){
+    if(operation_cb_){
+        operation_cb_(Operation::POWER);
+    }
+}
 
 
+void MainWindow::on_tb_divide_clicked(){
+    if(operation_cb_){
+        operation_cb_(Operation::DIVISION);
+    }
+}
 
 
+void MainWindow::on_tb_multiplicate_clicked(){
+    if(operation_cb_){
+        operation_cb_(Operation::MULTIPLICATION);
+    }
+}
 
 
+void MainWindow::on_tb_substract_clicked(){
+    if(operation_cb_){
+        operation_cb_(Operation::SUBTRACTION);
+    }
+}
 
 
+void MainWindow::on_tb_add_clicked(){
+    if(operation_cb_){
+        operation_cb_(Operation::ADDITION);
+    }
+}
 
+
+void MainWindow::on_tb_equal_clicked(){
+    if(control_cb_){
+        control_cb_(ControlKey::EQUALS);
+    }
+}
+
+
+void MainWindow::on_tb_reset_clicked(){
+    if(control_cb_){
+        control_cb_(ControlKey::CLEAR);
+        SetInputText("0");
+    }
+}
+
+
+void MainWindow::on_tb_ms_clicked(){
+    if(control_cb_){
+        control_cb_(ControlKey::MEM_SAVE);
+        ui->l_memory->setText("M");
+    }
+}
+
+
+void MainWindow::on_tb_mc_clicked(){
+    if(control_cb_){
+        control_cb_(ControlKey::MEM_CLEAR);
+        ui->l_memory->setText("");
+    }
+}
+
+//Не знаю, почему, но в видео примере эта кнопка MR,
+//хотя в ControlKey - ML и, чисто логически, Memory Load - ML
+//но оставлю, как есть в видео примере
+void MainWindow::on_tn_mr_clicked()
+{
+    if(control_cb_){
+        control_cb_(ControlKey::MEM_LOAD);
+    }
+}
+
+
+void MainWindow::on_cmb_controller_currentIndexChanged(){
+    if (controller_cb_) {
+        QString selectedText = ui->cmb_controller->currentText();
+        ControllerType controllerType;
+
+        if (selectedText == "double") {
+            controllerType = ControllerType::DOUBLE;
+        } else if (selectedText == "float") {
+            controllerType = ControllerType::FLOAT;
+        } else if (selectedText == "uint8_t") {
+            controllerType = ControllerType::UINT8_T;
+        } else if (selectedText == "int") {
+            controllerType = ControllerType::INT;
+        } else if (selectedText == "int64_t") {
+            controllerType = ControllerType::INT64_T;
+        } else if (selectedText == "size_t") {
+            controllerType = ControllerType::SIZE_T;
+        } else if (selectedText == "Rational") {
+            controllerType = ControllerType::RATIONAL;
+        }
+
+        controller_cb_(controllerType);
+    }
+}
---
+++ calculator/rational.h
@@ -0,0 +1,200 @@
+#pragma once
+#include <cstdint>
+#include <compare>
+#include <iostream>
+#include <numeric>
+#include <cstdlib>
+
+
+
+class Rational{
+public:
+    //Конструкторы
+    Rational() = default;
+
+    Rational(int numerator): numerator_(numerator){
+        denominator_ = 1;
+    }
+
+    Rational(int numerator, int denominator): numerator_(numerator), denominator_(denominator){
+        Reduction();
+    };
+
+    Rational(const Rational& r): numerator_(r.numerator_), denominator_(r.denominator_) {
+    };
+
+    //Перегрузка операторов
+    friend std::ostream& operator<<(std::ostream &os, const Rational& rational);
+    friend std::istream& operator>>(std::istream &is,  Rational& rational);
+
+
+    Rational operator+(const Rational& r) const{
+        int new_numerator = numerator_ * r.denominator_ + r.numerator_ * denominator_;
+        int new_denominator = denominator_ * r.denominator_;
+
+        Rational result(new_numerator, new_denominator);
+        result.Reduction();
+
+        return result;
+    }
+
+    Rational operator-(const Rational& r) const{
+        int new_numerator = numerator_ * r.denominator_ - r.numerator_ * denominator_;
+        int new_denominator = denominator_ * r.denominator_;
+
+        Rational result(new_numerator, new_denominator);
+        result.Reduction();
+
+        return result;
+    }
+
+    Rational operator*(const Rational& r) const{
+        int new_numerator = numerator_ * r.numerator_;
+        int new_denominator = denominator_ * r.denominator_;
+
+        Rational result(new_numerator, new_denominator);
+        result.Reduction();
+
+        return result;
+    }
+
+    Rational operator/(const Rational& r) const{
+        Rational result(*this);
+        result *= r.Inv();
+        result.Reduction();
+
+        return result;
+    }
+
+    Rational& operator+=(const Rational& r) {
+        numerator_ = numerator_ * r.denominator_ + r.numerator_ * denominator_;
+        denominator_ = denominator_ * r.denominator_;
+        Reduction();
+
+        return *this;
+    }
+
+    Rational& operator-=(const Rational& r){
+        numerator_ = numerator_ * r.denominator_ - r.numerator_ * denominator_;
+        denominator_ = denominator_ * r.denominator_;
+        Reduction();
+
+        return *this;
+    }
+
+    Rational& operator*=(const Rational& r){
+        numerator_ = numerator_ * r.numerator_;
+        denominator_ = denominator_ * r.denominator_;
+
+        Reduction();
+
+        return *this;
+    }
+
+    Rational& operator/=(const Rational& r){
+        numerator_ = numerator_ * r.denominator_;
+        denominator_ = denominator_ * r.numerator_;
+
+        Reduction();
+
+        return *this;
+    }
+
+    Rational& operator=(const Rational& r){
+        numerator_ = r.numerator_;
+        denominator_ = r.denominator_;
+
+        Reduction();
+
+        return *this;
+    }
+
+    const Rational& operator+() const{
+        return *this;
+    }
+
+    Rational operator-() const{
+        return Rational(-numerator_, denominator_);
+    }
+
+
+    bool operator==(const Rational& r) const = default;
+
+    auto operator<=>(const Rational& r2) const{
+
+        auto ls = static_cast<std::int64_t>(GetNumerator()) * static_cast<std::int64_t>(r2.GetDenominator());
+        auto rs = static_cast<std::int64_t>(r2.GetNumerator()) * static_cast<std::int64_t>(GetDenominator());
+
+        return ls <=> rs;
+    }
+
+    // методы
+    void Reduction() {
+        if (denominator_ < 0) {
+            numerator_ = -numerator_;
+            denominator_ = -denominator_;
+        }
+        const int divisor = std::gcd(numerator_, denominator_);
+        numerator_ /= divisor;
+        denominator_ /= divisor;
+    }
+
+    Rational Inv() const{
+        Rational result(denominator_, numerator_);
+
+        return result;
+    }
+
+    int GetNumerator() const{
+        return numerator_;
+    }
+
+    int GetDenominator() const{
+        return denominator_;
+    }
+
+private:
+    int numerator_ = 0;
+    int denominator_ = 1;
+};
+
+
+inline std::ostream& operator<<(std::ostream &os, const Rational &rational){
+    int numerator = rational.GetNumerator();
+    int denominator = rational.GetDenominator();
+
+
+    if(denominator == 1){
+        os << numerator;
+    }else {
+        os << numerator << " / " << denominator;
+    }
+
+    return os;
+}
+
+inline std::istream& operator>>(std::istream &is, Rational &rational){
+    int numerator, denominator;
+    char div;
+
+    if (!(is >> numerator)) {
+        is.setstate(std::ios::failbit);
+        return is; // Ошибка ввода числителя
+    }
+
+    // Проверка наличия разделителя
+    if (!(is >> std::ws >> div) || div != '/') {
+        denominator = 1; // Если разделитель не найден, знаменатель по умолчанию равен 1
+        is.clear();      // Сброс состояния ошибки потока
+    } else if  (!(is >> std::ws >> denominator) || denominator == 0){
+        is.setstate(std::ios::failbit);
+        return is; // Ошибка ввода знаменателя
+    }
+
+    rational.numerator_ = numerator;
+    rational.denominator_ = denominator;
+
+    rational.Reduction();
+
+    return is;
+}
